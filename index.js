0da9e3b4b74aecd79ca4272b3169e9e05096521f
Eyu-Gen <dreamersthebig@gmail.com>
2024-06-15 14:22:46 +0545

    Incomplete

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("markAll").addEventListener("click", function() {
+    document.getElementById("newNotificationNumber").style.display = "none";
+
+    let mark = document.getElementsByClassName("mark");
+    let box = document.getElementsByClassName("newNotification");
+
+    for(let i = 0; i < box.length; i++) {
+        mark[i].style.display = "none";
+        box[i].style.backgroundColor = "var(--defaultColor)";
+    }
+});
